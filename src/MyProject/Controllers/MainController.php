0b2dbd08a4a8f9cb427dd8fed00e112a0b0413e6
Olha Pushkar <olha.pushkar@scrumlaunch.com>
2023-06-28 22:25:58 +0300

    feat: add models, view, templates, routes

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -2,11 +2,23 @@
 
 namespace MyProject\Controllers;
 
+use MyProject\View\View;
+
 class MainController 
 {
+    private $view;
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../../../templates');
+    }
     public function main() 
     {
-        echo 'Main Page';
+        $articles = [
+            ['name' => 'Article 1', 'text' => 'Text of the article 1'],
+            ['name' => 'Article 2', 'text' => 'Text of the article 2']
+        ];
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
+        // include __DIR__ . '/../../../templates/main/main.php';
     }
 
     public function sayHello(string $name) 
